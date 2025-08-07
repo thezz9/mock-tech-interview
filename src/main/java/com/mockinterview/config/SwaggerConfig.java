738b4ce119389310362b367a6518c80886a42dbd
thezz9 <thezz9@naver.com>
2025-08-07 20:49:20 +0900

    feat: Swagger 설정을 어노테이션 기반으로 전환 및 JWT 보안 설정 추가

--- src/main/java/com/mockinterview/config/SwaggerConfig.java
+++ src/main/java/com/mockinterview/config/SwaggerConfig.java
@@ -1,20 +1,25 @@
 package com.mockinterview.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 
 @Configuration
+@OpenAPIDefinition(
+    info = @Info(
+        title = "Mock Tech Interview API",
+        version = "v1.0",
+        description = "모의 기술면접 API 문서"
+    )
+)
+@SecurityScheme(
+    name = "JWT",
+    type = SecuritySchemeType.HTTP,
+    scheme = "bearer",
+    bearerFormat = "JWT"
+)
 public class SwaggerConfig {
-
-    @Bean
-    public OpenAPI openAPI() {
-        return new OpenAPI()
-            .info(new Info()
-                .title("Mock Tech Interview API")
-                .description("모의 기술 면접 API 문서")
-                .version("1.0.0"));
-    }
 }
