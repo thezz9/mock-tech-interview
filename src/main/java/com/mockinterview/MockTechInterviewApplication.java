330271fa7378a99276604977b5430f6cdff6630e
thezz9 <thezz9@naver.com>
2025-08-07 11:10:31 +0900

    init: project setup

---
+++ src/main/java/com/mockinterview/MockTechInterviewApplication.java
@@ -0,0 +1,13 @@
+package com.mockinterview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockTechInterviewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MockTechInterviewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mockinterview/MockTechInterviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mockinterview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MockTechInterviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
